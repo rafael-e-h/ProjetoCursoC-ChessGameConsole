844076a9cf580be96bbddf1e794487966ff6d3ce
rafael-e-h <rafael-e-h@hotmail.com>
2023-09-22 18:49:51 -0300

    Improving the implementation of the printBoard method

--- ChessConsoleGame/Program.cs
+++ ChessConsoleGame/Program.cs
@@ -19,8 +19,9 @@ namespace xadrez_console
             tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
             tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
             tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 4));
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
 
-            Tela.imprimirTabuleiro(tab);              
+                Tela.imprimirTabuleiro(tab);              
 
             }
             catch (TabuleiroException e) {
--- ChessConsoleGame/Tela.cs
+++ ChessConsoleGame/Tela.cs
@@ -11,21 +11,69 @@ namespace ChessConsoleGame
     {
         public static void imprimirTabuleiro(Tabuleiro tab)
         {
-            for (int i = 0; i< tab.linhas; i++)
+            for (int i = 0; i < tab.linhas; i++)
             {
-                for (int j=0; j< tab.colunas; j++)
+                Console.BackgroundColor = System.ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+                Console.Write(8 - i + " ");
+                for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
                     {
-                        Console.Write("- ");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        imprimirPeca(null, "- ");
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
+
+            Console.BackgroundColor = System.ConsoleColor.Gray;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("  a b c d e f g h ");
+            
+
         }
+
+        public static void imprimirPeca(Peca peca, string outro = "")
+        {
+            if (peca != null)
+            {
+                if (peca.cor == Cor.Branca)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.Write(peca);
+
+                }
+                else if (peca.cor == Cor.Preta)
+                {
+                    //ConsoleColor aux = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write(peca);
+
+                    //Console.ForegroundColor = aux;
+                }
+            }
+            else
+            {
+                Console.BackgroundColor = System.ConsoleColor.DarkYellow;
+                Console.ForegroundColor = ConsoleColor.Gray;
+                if (outro != "")
+                {
+                    Console.Write(outro);
+                }
+            }
+            Console.BackgroundColor = System.ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
     }
 }

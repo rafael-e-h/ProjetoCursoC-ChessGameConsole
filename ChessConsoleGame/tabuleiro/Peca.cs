76f393ec8a95ac96c9893459357e7baac9b42441
rafael-e-h <rafael-e-h@hotmail.com>
2023-11-21 20:00:45 -0300

    Implemented the restriction of movement

--- ChessConsoleGame/tabuleiro/Peca.cs
+++ ChessConsoleGame/tabuleiro/Peca.cs
@@ -22,14 +22,36 @@ namespace ChessConsoleGame.tabuleiro
 
         }
 
-        public abstract bool[,] movimentosPossiveis();
-     
-
         public void incrementarQteMovimentos()
         {
             qteMovimentos++;
         }
 
+
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j<tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+
+        }
+
+        public bool podeMoverPara(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+        }
+
+        public abstract bool[,] movimentosPossiveis();
 
     }
 }
